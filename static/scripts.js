1ad31280b556c2e05a8d33f3e46d5ee8888fc7b6
Maciej Swierad <maciej.swierad2@mail.dcu.ie>
2021-03-11 18:57:19 +0000

    added sign out functionality to OAuth, got rid of non functioning javascript, added a sign out button

--- static/scripts.js
+++ static/scripts.js
@@ -4,27 +4,9 @@ function onSignIn(googleUser) {
 	console.log('Name: ' + profile.getName());
 }
 
-window.addEventListener("load", function() {
-	function send() {
-		const XHR = new XMLHttpRequest();
-
-		const FD = new FormData(form);
-		XHR.addEventListener("load", function(event) {
-			alert(event.target.responseText);
-		});
-
-		XHR.open("GET", "");
-		var name = profile.getName();
-		var splitname = name.split(" ");
-		var username= splitname.join("");
-		var dataAsString = new URLSearchParams(FormData).toString();
-		var data = dataAsString + "&user=" + username;
-		console.log(data);
-		XHR.send(data);
-	}
-	const form = document.getElementById("images");
-	form.addEventListener("submit", function(event) {
-		event.preventDefault();
-		send();
+function signOut() {
+	var session = gapi.auth2.getAuthInstance();
+	session.signOut().then(function () {
+		console.log('Signed out user.');
 	});
-});
+}
