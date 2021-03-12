b34fad7c436273e1af921f037170f41c7b40b8d9
Maciej Swierad <maciej.swierad2@mail.dcu.ie>
2021-03-12 12:18:51 +0000

    added new username cookie to javascript onsign in function

--- static/scripts.js
+++ static/scripts.js
@@ -2,6 +2,10 @@ function onSignIn(googleUser) {
 	profile = googleUser.getBasicProfile();
 	console.log('Email: ' + profile.getEmail());
 	console.log('Name: ' + profile.getName());
+	var name = profile.getName();
+	var splitname = name.split(" ");
+	var uname = splitname[0];
+	document.cookie = "username=" + uname + ";";
 }
 
 function signOut() {
